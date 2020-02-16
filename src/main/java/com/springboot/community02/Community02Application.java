d4660fabeea5c89c49577c2ad7aa331b9e400dbb
maomao <893987837@qq.com>
2020-02-16 17:17:26 +0800

    init rep

---
+++ src/main/java/com/springboot/community02/Community02Application.java
@@ -0,0 +1,13 @@
+package com.springboot.community02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/community02/Community02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.community02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Community02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
