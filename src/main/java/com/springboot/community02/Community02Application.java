25f2d7a25fa0d682a62055438bb852c6b9878394
maomao <893987837@qq.com>
2020-03-26 23:01:50 +0800

    add mybatis generator configuration

--- src/main/java/com/springboot/community02/Community02Application.java
+++ src/main/java/com/springboot/community02/Community02Application.java
@@ -1,9 +1,11 @@
 package com.springboot.community02;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.springboot.community02.mapper")
 public class Community02Application {
 
     public static void main(String[] args) {
