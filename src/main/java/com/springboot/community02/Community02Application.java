260c1701ea0de0a543ed80bc3c032712b7d1ed5e
maomao <893987837@qq.com>
2020-04-01 23:34:06 +0800

    add comment table

--- src/main/java/com/springboot/community02/Community02Application.java
+++ src/main/java/com/springboot/community02/Community02Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.springboot.community02.mapper")
 public class Community02Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Community02Application.class, args);
     }
-
 }
